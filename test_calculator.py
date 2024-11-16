d8243eb99742728483e757c7602310931a2a4033
Reem Hussin Mostafa Ibrahim <R.hussin2241@nu.edu.eg>
2024-11-16 18:24:39 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,15 @@
 import unittest
+from calculator import Calculator
 
 class TestCalculator(unittest.TestCase):
-    def test_add(self): self.assertEqual(calculator().add(2, 3), 5)
-    def test_add(self): self.assertEqual(calculator().add(6, 8), 14)
-    def test_add(self): self.assertEqual(calculator().add(8, 9), 17)
-    def test_add(self): self.assertEqual(calculator().add(500, 1000), 1500)
+    def setUp(self):
+        self.calculator = Calculator()
 
+    def test_add(self):
+        self.assertEqual(self.calculator.add(500, 1000), 1500)
+        self.assertEqual(self.calculator.add(2, 3), 5)
+        self.assertEqual(self.calculator.add(-2, 1), -1)
+        self.assertEqual(self.calculator.add(0, 0), 0)
 
+if __name__ == '__main__':
+    unittest.main()
